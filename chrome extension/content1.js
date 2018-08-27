21203ca509c3704e7edb3033e57683160afddced
Mohamed Fawaz K V <40982198+favazkandath@users.noreply.github.com>
2018-08-28 02:26:09 +0530

    Add files via upload
    updated content1

--- chrome extension/content1.js
+++ chrome extension/content1.js
@@ -1,21 +1,68 @@
+var tags;
+tags = document.getElementsByTagName("*");
+add();
+/*
+tags = document.getElementsByTagName("div");
+add();
+tags = document.getElementsByTagName("h1");
+add();
+tags = document.getElementsByTagName("head");
+add();
+tags = document.getElementsByTagName("a");
+add();
+tags = document.getElementsByTagName("h2");
+add();
+tags = document.getElementsByTagName("title");
+add();
+tags = document.getElementsByTagName("span");
+add();
+*/
 //console.log("content");
-var result = "";
-var content = document.body.innerText;
-//function fun1() {
-console.log("length = ", content.length);
-for (var element of content) {
-  if (
-    element == "a" ||
-    element == "e" ||
-    element == "i" ||
-    element == "o" ||
-    element == "u"
-  ) {
-    result = result + element.toUpperCase();
-  } else {
-    result = result + element;
+function add() {
+  //document.body.innerText = result;
+  //console.log(result);
+
+  for (var i = 0; i < tags.length; i++) {
+    var element = tags[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var oldText = element.childNodes[j];
+      if (oldText.nodeType == 3) {
+        var words = oldText.nodeValue.split(" ");
+        // console.log(words);
+        for (var a = 0; a < words.length; a++) {
+          words[a] = capitalize(words[a]);
+
+          var newText = words.join(" ");
+          // console.log("in function", newText);
+          //console.log("child", oldText.textContent);
+        }
+        element.replaceChild(document.createTextNode(newText), oldText);
+      }
+    }
   }
 }
 
-document.body.innerText = result;
+//document.body.innerText = result;
 //console.log(result);
+
+function capitalize() {
+  var word = arguments[0];
+  var wor = "";
+  for (var i = 0; i < word.length; i++) {
+    if (
+      word[i] == "a" ||
+      word[i] == "e" ||
+      word[i] == "i" ||
+      word[i] == "o" ||
+      word[i] == "u"
+    ) {
+      wor += word[i].toUpperCase();
+    } else {
+      wor += word[i];
+    }
+  }
+  //console.log(wor);
+
+  return wor;
+}
